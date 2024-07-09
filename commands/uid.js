e1d74c8919a26e6c34930800fcc34f2e344517e1
zane108231 <johnlongno4@gmail.com>
2024-07-09 20:13:52 +0800

    Update uid.js

--- commands/uid.js
+++ commands/uid.js
@@ -1,7 +1,10 @@
 module.exports = (bot) => {
   bot.onText(/\/uid/, (msg) => {
-    const chatId = msg.chat.id;
-    const uidMessage = `Your Uid is here mother fucker: ${chatId}`;
+    const chatId = msg.chat.id; // Get the chat ID
+    const userId = msg.from.id; // Get the user ID
+    const firstName = msg.from.first_name; // Get the user's first name (optional)
+
+    const uidMessage = `Your UID is here, ${firstName}: ${userId}`;
     bot.sendMessage(chatId, uidMessage);
   });
 };
